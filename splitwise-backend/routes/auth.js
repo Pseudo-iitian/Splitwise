a2977967a328689cd7f482851b1d920315ae4eb0
AbhishekINT153 <abhishek.verma.6561@gmail.com>
2026-05-01 15:16:08 +0530

    done

--- splitwise-backend/routes/auth.js
+++ splitwise-backend/routes/auth.js
@@ -3,7 +3,6 @@ const router   = express.Router();
 const bcrypt   = require('bcryptjs');
 const jwt      = require('jsonwebtoken');
 const User     = require('../models/User');
-
 router.post('/register', async (req, res) => {
   try {
     const { name, email, password } = req.body;
